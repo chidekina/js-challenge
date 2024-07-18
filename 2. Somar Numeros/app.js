57a757a9fef314cd1ede708db16a27f878f49e4c
chidekina <cesar.nagano1@gmail.com>
2024-07-18 13:36:16 -0300

    Update app.js

--- 2. Somar Numeros/app.js
+++ 2. Somar Numeros/app.js
@@ -1,38 +1,3 @@
-// Neste desafio você vai criar uma função capaz de receber dois argumentos e retornar a soma entre eles. Também vai precisar fazer validações para garantir que está somando parâmetros do tipo numérico.
-
-// Links que pode ajudar
-
-// Number
-// Number.isNaN()
-// if…else
-// Requisitos
-
-// Criar uma função com o nome somarNumeros
-// Quando a função for executada com dois argumentos do tipo numérico, deve ser retornado a soma entre os argumentos
-// A função deve ser capaz de fazer soma entre números positivos, negativos, inteiros e decimais
-// A função deve ser capaz de fazer soma entre números que chegam como string e garantir que vai retornar um número e não uma string concatenada
-// Quando um dos parâmetros não for um número, a função deve retornar a mensagem: "Função aceita apenas números."
-
-// somarNumeros({}, []);
-
-// Resultado: Função aceita apenas números ou valores que podem ser convertidos em números.
-
-// somarNumeros([], {});
-
-// Resultado: Função aceita apenas números ou valores que podem ser convertidos em números.
-
-// somarNumeros("a", "b");
-
-// Resultado: Função aceita apenas números ou valores que podem ser convertidos em números.
-
-// somarNumeros(1, []);
-
-// Resultado: Função aceita apenas números ou valores que podem ser convertidos em números.
-
-// somarNumeros({}, 10);
-
-// Resultado: Função aceita apenas números ou valores que podem ser convertidos em números.
-
 function somarNumeros (a,b){
     if (typeof a != 'number' && typeof a != 'string' && typeof a != 'boolean'){
         return 'Função aceita apenas números ou valores que podem ser convertidos em números.';
